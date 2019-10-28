efc917ffbfe861e0396f0788006619d47e60d86a
root <root@ip-172-31-34-141.eu-central-1.compute.internal>
2019-10-28 12:44:34 +0000

    minor corrections

--- setupDatabases.sh
+++ setupDatabases.sh
@@ -2,7 +2,7 @@
 
 #rm gdb.config 2> /dev/null
 
-find . -depth -name '*.zip' -exec /usr/bin/unzip -n {} \; -delete
+#find . -depth -name '*.zip' -exec /usr/bin/unzip -n {} \; -delete
 
 wget -q https://raw.githubusercontent.com/bridgedb/BridgeDb/master/dbbuilder/src/org/bridgedb/extract/SpeciesList -O SpeciesList
 
@@ -10,14 +10,15 @@ while IFS=$'\t' read -r -a myArray
 do
  #echo "a ${myArray[0]}"
  #echo "b ${myArray[1]}"
- #echo "c ${myArray[2]}"
+ #echo "c ${myArray[4]}"
 
  path=`pwd`
- file=`ls | grep "^${myArray[2]}"`
-
- #echo $path
- #echo -e "$file\n"
- echo -e "${myArray[2]}\n"
- #echo -e "${myArray[0]}\t$file" >> gdb.config
+ file=`ls | grep "^${myArray[4]}_Derby_"`
+ if [ ${#file} -ge 1 ]; then
+   #echo $path
+   #echo -e "file $file"
+   #echo -e "${myArray[2]}\n"
+   echo -e "${myArray[0]}\t$path/$file" >> gdb.config
+ fi
 
 done < SpeciesList
